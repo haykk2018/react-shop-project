252d55306ba2bd8be9a64cd9e573a70a68e0333f
hayk <haykk2018@gmail.com>
2023-03-14 22:48:51 +0400

    embed google icon api materialize css framework via <link href=

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <div className="container">
+            <span>Hello</span><span className="material-icons">waving_hand</span>
+            <div className="row">
+                <div className="col s12"><span
+                    className="flow-text">This div is 12-columns wide on all screen sizes</span></div>
+                <div className="col s6 offset-s6"><span className="flow-text">6-columns (offset-by-6)</span></div>
+            </div>
+        </div>
+    </>
 }
 
 export default App;
